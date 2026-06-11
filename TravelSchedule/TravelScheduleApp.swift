463a1b32bf4fb483e906fbf2193c3c1d72f52eba
Andrew Ruzavin <andrew.ruzavin@gmail.com>
2026-06-11 19:11:48 +0700

    initial commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by  Андрей Рузавин on 11/6/26.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
